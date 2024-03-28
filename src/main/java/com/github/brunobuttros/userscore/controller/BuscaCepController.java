d733fc3907dfed35ee8babfeb9e28b708da58fd6
Bruno Buttros <bruno.buttros@hotmail.com>
2024-03-27 22:48:14 -0300

    Criado tables para usuario e endereço

---
+++ src/main/java/com/github/brunobuttros/userscore/controller/BuscaCepController.java
@@ -0,0 +1,13 @@
+package com.github.brunobuttros.userscore.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/buscaCep")
+public class BuscaCepController {
+
+    // CRIAR SERVICE @Autowired EnderecoService enderecoService;
+
+
+}
