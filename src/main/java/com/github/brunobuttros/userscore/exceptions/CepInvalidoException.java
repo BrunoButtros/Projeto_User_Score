41a2541ff1b1734e52986186873339c248287fd5
Bruno Buttros <bruno.buttros@hotmail.com>
2024-03-28 14:47:10 -0300

    Adicionando exceções ao buscaCep e criando classe de exceptions

---
+++ src/main/java/com/github/brunobuttros/userscore/exceptions/CepInvalidoException.java
@@ -0,0 +1,8 @@
+package com.github.brunobuttros.userscore.exception;
+
+    public class CepInvalidoException extends RuntimeException {
+        public CepInvalidoException(String message) {
+            super(message);
+        }
+    }
+}
