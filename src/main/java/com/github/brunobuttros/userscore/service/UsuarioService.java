c05fa55f39731174d9f339e18899ad61fa88e44b
Bruno Buttros <bruno.buttros@hotmail.com>
2024-03-28 13:52:11 -0300

    Refazendo as alterações após um merge errado

--- src/main/java/com/github/brunobuttros/userscore/service/UsuarioService.java
+++ src/main/java/com/github/brunobuttros/userscore/service/UsuarioService.java
@@ -1,8 +1,49 @@
 package com.github.brunobuttros.userscore.service;
 
+import com.github.brunobuttros.userscore.dto.EnderecoDTO;
+import com.github.brunobuttros.userscore.dto.UsuarioDTO;
+import com.github.brunobuttros.userscore.entity.EnderecoEntity;
+import com.github.brunobuttros.userscore.entity.UsuarioEntity;
+import com.github.brunobuttros.userscore.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UsuarioService {
 
+    private final UsuarioRepository usuarioRepository;
+    private final BuscaCep buscaCep;
+
+    @Autowired
+    public UsuarioService(UsuarioRepository usuarioRepository, BuscaCep buscaCep) {
+        this.usuarioRepository = usuarioRepository;
+        this.buscaCep = buscaCep;
+    }
+
+    public UsuarioEntity cadastrarUsuario(UsuarioDTO usuarioDTO) {
+        UsuarioEntity usuario = new UsuarioEntity();
+        usuario.setNome(usuarioDTO.nome());
+        usuario.setEmail(usuarioDTO.email());
+        usuario.setTelefone(usuarioDTO.telefone());
+        usuario.setCpf(usuarioDTO.cpf());
+        usuario.setScore(usuarioDTO.score());
+
+        List<String> ceps = usuarioDTO.enderecos().stream().map(EnderecoDTO::cep).toList();
+        List<EnderecoEntity> enderecos = new ArrayList<>();
+        for (String cep : ceps) {
+            try {
+
+                EnderecoEntity endereco = buscaCep.getEnderecoEntity(cep);
+                enderecos.add(endereco);
+            } catch (Exception e) {
+                System.err.println("Erro ao buscar o CEP" + cep + ": " + e.getMessage());
+            }
+        }
+        usuario.setEnderecos(enderecos);
+        return usuarioRepository.save(usuario);
+
+    }
 }
